6beb8ffce024eccef93e657cbefd42d626a0cdf3
Kemjiga <kemerj@uninorte.edu.co>
2023-11-21 01:04:45 -0500

    feat: add gateway service

---
+++ gateway/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const proxy = require('express-http-proxy');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.status(200).json({ message: 'Root' });
+  });
+app.use('/create', proxy('http://localhost:8001'));
+app.use('/delete', proxy('http://localhost:8002'));
+app.use('/read', proxy('http://localhost:8003'));
+app.use('/update', proxy('http://localhost:8004'));
+
+
+app.listen(8000, () => {
+  console.log('Listening on port 8000');
+});
