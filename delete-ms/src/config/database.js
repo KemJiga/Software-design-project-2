897e0eb41ab21199e3cca9102d18c258e39ba268
Kemjiga <kemerj@uninorte.edu.co>
2023-11-22 14:43:10 -0500

    fix: change mongoUri

--- delete-ms/src/config/database.js
+++ delete-ms/src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const CONNECTION = process.env.MONGODB_URI || `mongodb://127.0.0.1:27017/businessDB`;
+const CONNECTION = process.env.MONGODB_URI || 'mongodb://Kemjiga:password@mongo-container:27017/miapp?authSource=admin';
 const connectDB = async (cb) => {
     try {
         await mongoose.connect(CONNECTION)
